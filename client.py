60b36f9d273495b06b9f5ac04c4ba612ac7f30e3
Evgeny Khaliper <eugen.none@gmail.com>
2018-11-04 09:12:07 +0200

    client accepts host

--- client.py
+++ client.py
@@ -1,32 +1,40 @@
 #!/usr/bin/env python
+import sys
+
 import urllib2
 from BaseHTTPServer import HTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 
-class MyHandler(SimpleHTTPRequestHandler):
+def HandlrFactory(server_address):
+    class Handlr(SimpleHTTPRequestHandler):
+        def __init__(self, request, client_address, server):
+            self.server_address = server_address
+            SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
 
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        def _set_headers(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
 
-    def do_GET(self):
-        if self.path == "/health-check":
+        def do_GET(self):
+            if self.path == "/health-check":
+                self._set_headers()
+            elif self.path == "/":
+                self._set_headers()
+                req = urllib2.Request('http://{}:3001'.format(self.server_address))
+                response = urllib2.urlopen(req)
+                content = response.read()
+                self.wfile.write(content)
+            else:
+                self.send_error(404)
+
+        def do_HEAD(self):
             self._set_headers()
-        elif self.path == "/":
-            self._set_headers()
-            req = urllib2.Request('http://localhost:3001')
-            response = urllib2.urlopen(req)
-            content = response.read()
-            self.wfile.write(content)
-        else:
-            self.send_error(404)
 
-    def do_HEAD(self):
-        self._set_headers()
+    return Handlr
 
 
 if __name__ == '__main__':
-    client = HTTPServer(('127.0.0.1', 3000), MyHandler)
+    client = HTTPServer(('127.0.0.1', 3000), HandlrFactory(sys.argv[1]))
     client.serve_forever()
